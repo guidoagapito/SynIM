56de72222b696aae0298eb61ea75e8c96205c18f
Guido Agapito <guido.agapito@inaf.it>
2025-04-19 00:03:29 +0200

    (GA) uses compute_interaction_matrices function

--- run_synim_from_yaml_ngs.py
+++ run_synim_from_yaml_ngs.py
@@ -1,9 +1,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from utils.params_utils import parse_params_file, compute_interaction_matrix,\
-                               prepare_interaction_matrix_params, generate_im_filename
-
+from utils.params_utils import compute_interaction_matrices
+from utils.params_common_utils import generate_im_filename
 import specula
 specula.init(device_idx=-1, precision=1)
 
@@ -16,92 +15,50 @@ specula_init_path = specula.__file__
 specula_package_dir = os.path.dirname(specula_init_path)
 specula_repo_path = os.path.dirname(specula_package_dir)
 
-
 # Path to the YAML configuration file and output directory
 # The path to the YAML file is determined by the specula module
-yaml_file = os.path.join(specula_repo_path, "main", "scao", "params_morfeo_full2.yml")
+yaml_file = "/Users/guido/GitHub/SPECULA_scripts/params_morfeo_full2.yml"
+root_dir = os.path.join(specula_repo_path, "main", "scao","calib","MCAO")
 print(f"YAML file path: {yaml_file}")
 #output directory is set to the caibration directory of the SPECULA repository
 output_im_dir = os.path.join(specula_repo_path, "main", "scao", "calib", "MCAO", "im")
 output_rec_dir = os.path.join(specula_repo_path, "main", "scao", "calib", "MCAO", "rec")
 print(f"Output directory: {output_im_dir}")
 
-# -------------------------------------------------------------------
-# Make sure the output directory exists
-os.makedirs(output_im_dir, exist_ok=True)
-os.makedirs(output_rec_dir, exist_ok=True)
+matrices = compute_interaction_matrices(yaml_file, root_dir=root_dir, output_im_dir=output_im_dir, 
+                                 wfs_type='ngs', overwrite=True, verbose=True, display=False)
 
 # -------------------------------------------------------------------
-# Load the YAML or PRO file
-paramsAll = parse_params_file(yaml_file)
-# Set the root directory to the absolute path of the specula repository
-paramsAll['main']['root_dir'] = os.path.join(specula_repo_path, "main", "scao","calib","MCAO")
-print(f"Absolute path of root directory: {paramsAll['main']['root_dir']}")
-
-# for cycle on all WFS, 3, and DM, 3
+# Load from disk the full set of interaction matrices
+# then put them in a singla 2D array NXM
+# where N is the number of modes, 2 for the first DM and 3 for the third DM
+# and M is the number of slopes, 8 multiplied by 3 WFSs
+N = 5
+n_slopes = 2
+M = 3*n_slopes
+im_full = np.zeros((N,M)) 
 for ii in range(3):
     for jj in range(3):
-        
-        print(f"Processing WFS {ii+1} and DM {jj+1}")
-
+        if jj == 1:
+            continue
         im_filename = generate_im_filename(yaml_file, wfs_type='ngs', wfs_index=ii+1, dm_index=jj+1)
         # Full paths for the files
         im_path = os.path.join(output_im_dir, im_filename)
-
         print(f"--> Generated IM filename: {im_filename}")
-        # -------------------------------------------------------------------
+        # Load the interaction matrix
+        intmat_obj = Intmat.restore(im_path)
+        # Get the interaction matrix data
+        if jj == 0:
+            mode_idx = [0,1]
+        if jj == 2:
+            mode_idx = [2,3,4]
+        print(f'size of intmat: {intmat_obj._intmat.shape}')
+        im_full[mode_idx, n_slopes*ii:n_slopes*(ii+1)] = intmat_obj._intmat[mode_idx,:]
         
-        params = prepare_interaction_matrix_params(paramsAll,wfs_type='ngs',wfs_index=ii+1,dm_index=jj+1)
-
-        # -------------------------------------------------------------------
-        # rotate the DM array and mask to be coherent with the specula convention
-        params['dm_array'] = params['dm_array'].transpose(1, 0, 2)
-        params['dm_mask'] = np.transpose(params['dm_mask'])
-        params['pup_mask'] = np.transpose(params['pup_mask'])
-
-        # rotate the list of valid subapertures to be coherent with the specula convention
-        sa2D = np.zeros((params['wfs_nsubaps'],params['wfs_nsubaps']))
-        sa2D[params['idx_valid_sa'][:,0], params['idx_valid_sa'][:,1]] = 1
-        sa2D = np.transpose(sa2D)
-        idx_valid_sa_new = np.where(sa2D>0)
-        idx_valid_sa = params['idx_valid_sa']
-        idx_valid_sa[:,0] = idx_valid_sa_new[0]
-        idx_valid_sa[:,1] = idx_valid_sa_new[1]
-        params['idx_valid_sa'] = idx_valid_sa
-        # -------------------------------------------------------------------
-
-        print(f"--> Parameters for interaction matrix computation: {params}")
-
-        # Calculate the interaction matrix
-        im = compute_interaction_matrix(params, verbose=True, display=False)
-
-        # transpose to be coherent with the specula convention
-        im = im.transpose()*2*np.pi
-        # change x and y to be coherent with the specula convention
-        print('im.shape',im.shape)
-        right_half = im[:, int(im.shape[1] / 2):]
-        left_half = im[:, :int(im.shape[1] / 2)]
-        im = np.concatenate((right_half, left_half), axis=1)
-        print('im.shape',im.shape)
-
-        # Create the Intmat object
-        # Extract WFS type info for pupdata_tag
-        wfs_info = f"{params['wfs_type']}_{params['wfs_nsubaps']}"
-        pupdata_tag = f"{os.path.basename(yaml_file).split('.')[0]}_{wfs_info}"
-
-        # Create Intmat object and save it
-        intmat_obj = Intmat(
-            im, 
-            pupdata_tag=pupdata_tag,
-            norm_factor=1.0,  # Default value
-            target_device_idx=None,  # Use default device
-            precision=None    # Use default precision
-        )
-
-        # Save the interaction matrix
-        intmat_obj.save(im_path)
-        print(f"Interaction matrix saved as: {im_path}")
-        # -------------------------------------------------------------------
+import pandas as pd
+print("Full interaction matrix:")
+df = pd.DataFrame(im_full)
+print(df.to_string(float_format=lambda x: f"{x:.6e}"))
 
 # TODO: computation of rec matrix with MMSE
 # rec_filename = im_filename.replace('IM', 'REC')
@@ -115,35 +72,17 @@ for ii in range(3):
 # print(f"Reconstruction matrix saved as: {rec_path}")
 
 # Print some statistics about the matrices
-print(f"Interaction matrix shape: {im.shape}")
-print(f"Interaction matrix dtype: {im.dtype}")
-print(f"Interaction matrix min: {im.min()}")
-print(f"Interaction matrix max: {im.max()}")
-print(f"Interaction matrix mean: {im.mean()}")
+print(f"Interaction matrix shape: {im_full.shape}")
+print(f"Interaction matrix dtype: {im_full.dtype}")
+print(f"Interaction matrix min: {im_full.min()}")
+print(f"Interaction matrix max: {im_full.max()}")
+print(f"Interaction matrix mean: {im_full.mean()}")
 # -------------------------------------------------------------------
 
 # Visualize the matrix
 plt.figure(figsize=(10, 8))
-plt.imshow(im, cmap='viridis')
+plt.imshow(im_full, cmap='viridis')
 plt.colorbar()
 plt.title(f"Interaction Matrix")
 plt.tight_layout()
-
-plt.figure(figsize=(8, 12))
-imBig = None
-for i in range(4):
-    im2Dx = np.zeros((params['wfs_nsubaps'],params['wfs_nsubaps']), dtype=im.dtype)
-    im2Dx[params['idx_valid_sa'][:,0], params['idx_valid_sa'][:,1]] = im[i,:params['idx_valid_sa'].shape[0]]
-    im2Dy = np.zeros((params['wfs_nsubaps'],params['wfs_nsubaps']), dtype=im.dtype)
-    im2Dy[params['idx_valid_sa'][:,0], params['idx_valid_sa'][:,1]] = im[i,params['idx_valid_sa'].shape[0]:]
-    im2D = np.concatenate((im2Dx, im2Dy), axis=1)
-    if imBig is None:
-        imBig = im2D
-    else:
-        imBig = np.concatenate((imBig, im2D), axis=0)
-plt.imshow(imBig, cmap='viridis')
-plt.colorbar()
-plt.title('Interaction Matrix 2D')
-plt.tight_layout()
-
 plt.show()
