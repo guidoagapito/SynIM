cb12e4d2d402647274a30b65fa09c0dadd674380
Guido Agapito <guido.agapito@inaf.it>
2025-04-18 18:39:05 +0200

    (GA) scale factor

--- synim.py
+++ synim.py
@@ -935,9 +935,9 @@ def interaction_matrix(pup_diam_m,pup_mask,dm_array,dm_mask,dm_height,dm_rotatio
     der_dx = apply_mask(der_dx,trans_pup_mask)
     der_dy = apply_mask(der_dy,trans_pup_mask)
 
-    scale_array = (der_dx.shape[0]/wfs_nsubaps)/np.median(rebin(trans_pup_mask, (wfs_nsubaps,wfs_nsubaps), method='average'))
-    WFS_signal_x = rebin(der_dx, (wfs_nsubaps,wfs_nsubaps), method='average')*scale_array[:,:,np.newaxis]
-    WFS_signal_y = rebin(der_dy, (wfs_nsubaps,wfs_nsubaps), method='average')*scale_array[:,:,np.newaxis]
+    scale_factor = (der_dx.shape[0]/wfs_nsubaps)/np.median(rebin(trans_pup_mask, (wfs_nsubaps,wfs_nsubaps), method='average'))
+    WFS_signal_x = rebin(der_dx, (wfs_nsubaps,wfs_nsubaps), method='average')*scale_factor
+    WFS_signal_y = rebin(der_dy, (wfs_nsubaps,wfs_nsubaps), method='average')*scale_factor
 
     debug_rebin_plot = False
     if debug_rebin_plot:
