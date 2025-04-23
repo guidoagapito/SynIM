eb8a16324e99f026b05040edabe0d9386432a7bb
Guido Agapito <guido.agapito@inaf.it>
2025-04-23 10:59:03 +0200

    (GA) moved to utils.py

--- synim/utils.py
+++ synim/utils.py
@@ -3,8 +3,9 @@ import re
 import yaml
 import datetime
 import numpy as np
-import matplotlib.pyplot as plt
-import synim.synim as synim
+
+from scipy.special import eval_legendre
+from scipy.ndimage import rotate
 
 import specula
 specula.init(device_idx=-1, precision=1)
@@ -15,7 +16,371 @@ from specula.data_objects.ifunc import IFunc
 from specula.data_objects.m2c import M2C
 from specula.data_objects.pupilstop import Pupilstop
 from specula.data_objects.subap_data import SubapData
-from specula.data_objects.intmat import Intmat
+
+def rebin(array, new_shape, method='average'):
+    """
+    The rebin function resizes a vector or array to dimensions given by the parameters new_shape.
+    In case of a 3D array the third dimension is not affected.
+
+    Parameters:
+    - array: numpy 2D or 3D array
+    - new_shape: 2 elements tuple
+    - method: 'sum' or 'average', used in the compression case
+
+    Returns:
+    - rebinned_array: numpy 2D or 3D array
+    """
+        
+    if array.ndim == 1:
+        array = array.reshape((-1, 1))  # Convert 1D array to 2D
+        
+    shape = array.shape
+    m, n = shape[0:2]
+    M, N = new_shape      
+
+    if M > m or N > n:
+        if M % m != 0 or N % n != 0:
+            raise ValueError("New shape must be multiples of the input dimensions.")
+
+        m_factor, n_factor = M // m, N // n
+
+        # Replicate the array in both dimensions
+        if len(shape) == 3:
+            rebinned_array = np.tile(array, (m_factor, n_factor, 1))
+        else:
+            rebinned_array = np.tile(array, (m_factor, n_factor))
+    else:    
+        if method == 'sum':
+            if len(shape) == 3:
+                rebinned_array = array[:M*(m//M), :N*(n//N), :].reshape((M, m//M, N, n//N, shape[2])).sum(axis=(1, 3))
+            else:
+                rebinned_array = array[:M*(m//M), :N*(n//N)].reshape((M, m//M, N, n//N)).sum(axis=(1, 3))
+        elif method == 'average':
+            if len(shape) == 3:
+                rebinned_array = array[:M*(m//M), :N*(n//N), :].reshape((M, m//M, N, n//N, shape[2])).mean(axis=(1, 3))
+            else:
+                rebinned_array = array[:M*(m//M), :N*(n//N)].reshape((M, m//M, N, n//N)).mean(axis=(1, 3))
+        else:
+            raise ValueError("Unsupported rebin method. Use 'sum' or 'average'.")  
+        
+    return rebinned_array
+
+def polar_to_xy(r,theta):
+    # conversion polar to rectangular coordinates
+    # theta is in rad
+    return np.array(( r * np.cos(theta),r * np.sin(theta) ))
+
+def xy_to_polar(x, y):
+    # conversion rectangular to polar coordinates
+    # theta is in rad
+    r = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    return r, theta
+
+def apply_mask(array,mask,norm=False):
+    # multiply a 2D or 3D by a 2D mask
+    # if norm is True 1/mask is used.
+    norm_array = np.copy(mask)
+    if norm:
+        idx2D = np.array(np.where(norm_array == 0))
+        norm_array[idx2D[0,:],idx2D[1,:]] = 1
+        norm_array = norm_array**(-1)
+    if len(array.shape) == 3:
+        norm_array_1d = norm_array.flatten()
+        array_2d = array.reshape((-1,array.shape[2]))
+        array_2d = array_2d*norm_array_1d[:, np.newaxis]
+        new_array = array_2d.reshape(array.shape)
+        if norm:
+            new_array[idx2D[0,:],idx2D[1,:],:] = 0
+    else:
+        new_array = array * norm_array
+        if norm:
+            new_array[idx2D[0,:],idx2D[1,:]] = 0
+    return new_array
+
+def make_xy(sampling, ratio, is_polar=False, is_double=False, is_vector=False,
+            use_zero=False, quarter=False, fft=False):
+    """
+    This function generates zero-centered domains in cartesian plane or axis, tipically for pupil sampling
+    and FFT usage. Converted from Armando Riccardi IDL make_xy procedure of IdlTools/oaa_lib/utilities library.
+
+    Parameters:
+    - sampling: number of points on the side ot he output arrays
+    - ratio: maximum value on the output arrays
+    - ...
+
+    Returns:
+    - x: numpy 2D array
+    - y: numpy 2D array
+    """    
+    
+    if sampling <= 1:
+        raise ValueError("make_xy -- sampling must be larger than 1")
+
+    if quarter:
+        if sampling % 2 == 0:
+            size = sampling // 2
+            x0 = 0.0 if use_zero else -0.5
+        else:
+            size = (sampling + 1) // 2
+            x0 = 0.0
+    else:
+        size = sampling
+        x0 = (sampling - 1) / 2.0 if is_double else (sampling - 1) / 2
+
+        if sampling % 2 == 0 and use_zero:
+            x0 += 0.5
+
+    ss = float(sampling)
+
+    x = (np.arange(size) - x0) / (ss / 2) * ratio
+
+    if not quarter:
+        if sampling % 2 == 0 and fft:
+            x = np.roll(x, -sampling // 2)
+        elif sampling % 2 != 0 and fft:
+            x = np.roll(x, -(sampling - 1) // 2)
+
+    if not is_vector or is_polar:
+        y = rebin(x, (size, size), method='average')
+        x = np.transpose(y)
+        if is_polar:
+            r, theta = xy_to_polar(x, y)
+            return r, theta
+
+    if is_vector:
+        y = x
+
+    return x, y
+
+def make_mask(npoints, obsratio=None, diaratio=1.0, xc=0.0, yc=0.0, square=False, inverse=False, centeronpixel=False):
+    """
+    This function generates nn array representing a mask.
+    Converted from Lorenzo Busoni IDL make_mask function of IdlTools/oaa_lib/ao_lib library.
+
+    Parameters:
+    - npoints: number of points on the side ot he output arrays
+    - obsratio: relative size of obscuration
+    - diaratio: relative size of diameter
+    - ...
+
+    Returns:
+    - mask: numpy 2D array
+    """
+    
+    x, y = np.meshgrid(np.linspace(-1, 1, npoints), np.linspace(-1, 1, npoints))
+
+    if xc is None:
+        xc = 0.0
+    if yc is None:
+        yc = 0.0
+    if obsratio is None:
+        obsratio = 0.0
+    ir = obsratio
+
+    if centeronpixel:
+        idx = np.argmin(np.abs(xc - x[0, :]))
+        idxneigh = np.argmin(np.abs(xc - x[0, idx - 1:idx + 2]))
+        k = -0.5 if idxneigh == 0 else 0.5
+        xc = x[0, idx] + k * (x[0, 1] - x[0, 0])
+
+        idx = np.argmin(np.abs(yc - y[:, 0]))
+        idxneigh = np.argmin(np.abs(yc - y[idx - 1:idx + 2, 0]))
+        k = -0.5 if idxneigh == 0 else 0.5
+        yc = y[idx, 0] + k * (y[1, 0] - y[0, 0])
+
+    if square:
+        mask = ((np.abs(x - xc) <= diaratio) & (np.abs(y - yc) <= diaratio) & 
+                ((np.abs(x - xc) >= diaratio * ir) | (np.abs(y - yc) >= diaratio * ir))).astype(np.uint8)
+    else:
+        mask = (((x - xc)**2 + (y - yc)**2 < diaratio**2) & 
+                ((x - xc)**2 + (y - yc)**2 >= (diaratio * ir)**2)).astype(np.uint8)
+
+    if inverse:
+        mask = 1 - mask
+
+    return mask
+
+def make_orto_modes(array):
+    # return an othogonal 2D array
+    
+    size_array = np.shape(array)
+
+    if len(size_array) != 2:
+        raise ValueError('Error in input data, the input array must have two dimensions.')
+
+    if size_array[1] > size_array[0]:
+        Q, R = np.linalg.qr(array.T)
+        Q = Q.T
+    else:
+        Q, R = np.linalg.qr(array)
+    
+    return Q
+
+def zern_degree(j):
+    """
+    From Armando Riccardi IDL function.
+    """
+    # return the zernike degree
+    n = np.floor(0.5 * (np.sqrt(8 * j - 7) - 3)) + 1
+    cn = n * (n + 1) / 2 + 1
+
+    if np.isscalar(n):
+        if n % 2 == 0:
+            m = np.floor((j - cn + 1) / 2) * 2
+        else:
+            m = np.floor((j - cn) / 2) * 2 + 1
+    else:
+        # new code for j vector
+        idx_even = np.where(n % 2 == 0)[0]
+        idx_odd = np.where((n + 1) % 2 == 0)[0]
+        m = n * 0
+        temp = j - cn
+        m[idx_even] = np.floor((temp[idx_even] + 1) / 2) * 2
+        m[idx_odd] = np.floor(temp[idx_odd] / 2) * 2 + 1
+
+    return n, m.astype(int)
+
+def zern_jradial(n, m, r):
+    """
+    Compute radial Zernike polynomial. 
+    From Armando Riccardi IDL function.
+
+    Parameters:
+    - n: int, radial order
+    - m: int, azimuthal order (0 <= m <= n, n-m even)
+    - r: numpy array, radial coordinate
+
+    Returns:
+    - jpol: numpy array, radial Zernike polynomial
+    """
+
+    if n < 0 or m < 0 or m > n or (n - m) % 2 != 0:
+        raise ValueError("Invalid values for n and m.")
+
+    nmm2 = (n - m) // 2
+
+    if m == 0:
+        return eval_legendre(nmm2, 2.0 * r ** 2 - 1.0)
+    else:
+        prefactor = np.sqrt(2.0 / (1.0 + 2.0 * n))
+        return prefactor * r ** m * eval_legendre(nmm2, 2.0 * r ** 2 - 1.0)
+
+def zern_jpolar(j, rho, theta):
+    """
+    Compute polar Zernike polynomial.
+    From Armando Riccardi IDL function.
+
+    Parameters:
+    - j: int, index of the polynomial, j >= 1
+    - rho: numpy array, point to evaluate (polar coord.)
+    - theta: numpy array, 
+
+    Returns:
+    - jpol: numpy 2D array, j-th Zernike polynomial in the point of polar coordinates r, theta
+    """
+    
+    if j < 1:
+        print("zern_jpolar -- must have j >= 1")
+        return 0.0
+
+    n, m = zern_degree(j)
+
+    result = np.sqrt(n + 1 + rho ** 2) * zern_jradial(n, m, rho)
+
+    if m == 0:
+        return result
+    elif j % 2 == 0:
+        return np.sqrt(2) * result * np.cos(m * theta)
+    else:
+        return np.sqrt(2) * result * np.sin(m * theta)
+
+def zern(j, x, y):
+    """
+    Compute the value of J-th Zernike polynomial in the points of coordinates x,y.
+    From Armando Riccardi IDL function.
+
+    Parameters:
+    - j: int, index of the polynomial, j >= 1
+    - rho: numpy 2D array, X coordinates
+    - theta: numpy 2D array, X coordinates
+
+    Returns:
+    - jzern: numpy 2D array, j-th Zernike polynomial in the point of coordinates x ,y
+    """
+
+    rho = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    return zern_jpolar(j, rho, theta)
+
+def zern2phi(dim, maxZernNumber, mask=None, no_round_mask=False, xsign=1, ysign=1, rot_angle=0, verbose=False):
+    """
+    Computes the Zernike phase cube and orthonormalizes it on a desired pupil (mask).
+    From Guido Agapito IDL function.
+
+    Parameters:
+    - dim: int, number of point on the side of the output array
+    - maxZernNumber: int, number of zernike modes excluding piston
+    - mask: optional, numpy 2D array, mask
+    - xsign: optional, sign of the x axis (for Zernike computation with zern function)
+    - ysign: optional, sign of the y axis (for Zernike computation with zern function)
+    - rot_angle: optional, float, rotation in deg
+    - verbose, optional
+
+    Returns:
+    - z2phi: numpy 3D array, set of maxZernNumber zernike modes
+    """
+    
+    if not no_round_mask:
+        round_mask = np.array(make_mask(dim))
+    else:
+        round_mask = np.ones((dim, dim), dtype=float)
+
+    if verbose:
+        print('Computing Zernike cube...')
+
+    xx, yy = make_xy(dim, 1, is_polar=False, is_double=False, is_vector=False,
+            use_zero=False, quarter=False, fft=False)
+    
+    z2phi = np.zeros((dim, dim, maxZernNumber + 1), dtype=float)
+
+    for i in range(maxZernNumber + 1):
+        zern_shape = zern(i + 1, xsign * xx, ysign * yy)
+        if rot_angle != 0:
+            zern_shape = rotate(zern_shape, rot_angle, axes=(1, 0), reshape=False)
+        z2phi[:, :, i] = zern_shape * round_mask
+
+    if verbose:
+        print('... Zernike cube computed')
+
+    if mask is not None:
+        if verbose:
+            print('Orthogonalizing Zernike cube...')
+
+        idx1D = np.where(mask.flatten())
+        idx2D = np.where(mask)
+
+        z2phi_temp = z2phi.reshape(-1, maxZernNumber + 1)
+        z2phi_on_pupil = z2phi_temp[idx1D,:]
+        z2phi_on_pupil = z2phi_on_pupil.reshape(-1,maxZernNumber + 1)
+        
+        z2phi_matrix_ortho = make_orto_modes(z2phi_on_pupil)
+
+        #z2phi = np.zeros((dim, dim, maxZernNumber + 1), dtype=float)
+        z2phi = np.full((dim,dim, maxZernNumber + 1),np.nan)
+        
+        for i in range(maxZernNumber + 1):
+            temp = np.zeros((dim, dim), dtype=float)
+            temp[idx2D[0],idx2D[1]] = z2phi_matrix_ortho[:, i] * 1/np.std(z2phi_matrix_ortho[:, i])
+            temp[idx2D[0],idx2D[1]] = temp[idx2D[0],idx2D[1]] - np.mean(temp[idx2D[0],idx2D[1]])
+            z2phi[:, :, i] = temp
+
+        if verbose:
+            print('Zernike cube orthogonalized!')
+
+    z2phi = z2phi[:, :, 1:]
+
+    return z2phi
 
 def is_simple_config(config):
     """
