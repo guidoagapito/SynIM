34c37cf419cfc985b749011b25818c1419b845bf
Guido Agapito <guido.agapito@inaf.it>
2025-11-06 20:22:35 +0100

    (GA) log scale

--- synim/synim.py
+++ synim/synim.py
@@ -1710,8 +1710,10 @@ def projection_matrix(pup_diam_m, pup_mask, dm_array, dm_mask, base_inv_array,
 
         # Display projection coefficients
         plt.figure(figsize=(10, 6))
+        x = np.arange(projection.shape[0])+1
         for i in range(min(5, projection.shape[1])):
-            plt.plot(projection[:, i], label=f'Basis mode {i}', marker='o', markersize=3)
+            plt.plot(x, projection[:, i], label=f'Basis mode {i}', marker='o', markersize=3)
+        plt.x_scale('log')
         plt.legend()
         plt.title('Projection Coefficients')
         plt.xlabel('DM mode index')
